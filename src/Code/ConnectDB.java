1f3c882c54d5ceb4c05600c5b39ea8f16e10f0e8
Tawhan Studio <36127485+tawhan695@users.noreply.github.com>
2018-04-19 23:50:35 +0700

    11*
    11%

--- src/Code/ConnectDB.java
+++ src/Code/ConnectDB.java
@@ -1,6 +1,7 @@
 
 package Code;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -28,13 +29,44 @@ public class ConnectDB {
  
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-           Connection  connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:tawhan", "sys as SYSDBA","695");
+           Connection  connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:tawhan", "tawhan as SYSDBA","695");
            
             if (connection !=null) {
                 System.out.println("เชื่อต่อเรียบร้อย "+connection.getSchema());
               
             }else System.out.println("not connection");
-                     
+             
+            //เพิ่มเข้าในตาราง
+           /* ********************************************************************
+            Statement sm=connection.createStatement();
+            String sql = "INSERT INTO customer " 
+                   
+              sm.execute(sql);
+            sm.close();
+             //select*************************************************************
+             ResultSet rec = sm.executeQuery(sql);
+            while((rec!=null) && (rec.next())){
+            System.out.print(rec.getString("CUSTOMERID"));
+            }**********************************************************************
+            สร้างชุดคำสั่งของ SQL ไว้สำหรับการ Update ข้อมูล
+
+            s.execute(sql);
+            *********************************************************************
+            	String sql = "UPDATE customer " +
+					"SET BUDGET = '5000000' " +
+					" WHERE CUSTOMERID = 'C005' ";
+             s.execute(sql);
+            *********************************************************************
+            DELEte
+            String sql = "DELETE FROM customer " +
+					" WHERE CUSTOMERID = 'C005' ";
+             s.execute(sql);
+            *********************************************************************
+            */
+          
+           /*
+          
+           */
         } catch (Exception e) {
             System.out.println("ERROR "+e);
         }
